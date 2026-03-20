b7cc6498fb50434d810e08579c4c005b2dc36603
PipororonoaZoro <maciel_ponce_9989@outlook.com.ar>
2026-03-20 20:45:11 -0300

    Agregando nuevas funciones al servidor

---
+++ services/superheroesService.mjs
@@ -0,0 +1,21 @@
+import superHeroRepository from '../repositories/SuperHeroRepository.mjs';
+
+export async function obtenerSuperheroePorId(id)
+{
+    return await superHeroRepository.obtenerPorId(id);
+}
+
+export async function obtenerTodosLosSuperheroes()
+{
+    return await superHeroRepository.obtenerTodos();
+}
+
+export async function buscarSuperHeoresPorAtributo(atributo, valor)
+{
+    return await superHeroRepository.buscarPorAtributos(atributo, valor);
+}
+
+export async function obtenerSuperheroesMayoresDe30()
+{
+    return await superHeroRepository.obtenerMayoresDe30();
+}
