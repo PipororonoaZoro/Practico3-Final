4422b54396615bcb8f754a091e67999d64a2cfc7
PipororonoaZoro <maciel_ponce_9989@outlook.com.ar>
2026-03-26 18:42:08 -0300

    cambios nuevos y modificando errores

--- repositories/SuperHeroRepository.mjs
+++ repositories/SuperHeroRepository.mjs
@@ -13,11 +13,13 @@ class SuperHeroRepository extends IRepository
         return await SuperHero.find({});
     }
 
+    // Resolución: Buscar por atributo
     async buscarPorAtributo(atributo, valor)
     {
         return await SuperHero.find({ [atributo]: valor });
     }
 
+    // Resolución: obtener mayores de 30
     async obtenerMayoresDe30()
     {
         return await SuperHero.find({ edad: { $gt: 30 } });
