89d056fc4b6fb61a6bddb5a3263b570ddac40544
PipororonoaZoro <maciel_ponce_9989@outlook.com.ar>
2026-04-07 00:23:16 -0300

    cambios

--- models/SuperHero.mjs
+++ models/SuperHero.mjs
@@ -2,12 +2,35 @@ import mongoose from 'mongoose';
 
 const superheroSchema = new mongoose.Schema(
 {
-    nombreSuperHeroe: { type: String, required:true },
-    nombreReal: { type: String, required: true },
-    edad: { type: Number, min: 0 },
+    nombreSuperHeroe: 
+    {   
+        type: String, 
+        required:true,
+        trim: true,
+        minlength: 3,
+        maxlength: 60 
+    },
+    nombreReal: 
+    { 
+        type: String, 
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 60 
+    },
+    edad: 
+    {   type: Number,
+        required: true,
+        min: 0 
+    },
     planetaOrigen: { type: String, default: 'Desconocido' },
     debilidad: String,
-    poderes: [String],
+    poderes: 
+    {   
+        type: [String],
+        required: true,
+        validate: { validator: function(arr) {return arr.length > 0 && arr.every(p => p.trim().length >= 3 && p.trim().length <= 60)}, message: 'Cada poder debe tener entre 3 y 60 caracteres' }
+    },
     aliados: [String],
     enemigos: [String],
     creador: String,
